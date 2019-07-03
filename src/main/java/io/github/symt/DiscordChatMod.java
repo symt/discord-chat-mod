214a18d597f5745490404cb1051ba1f07597bac2
Symt <xxrequired@gmail.com>
2019-07-02 22:24:05 -0500

    fixing version

--- src/main/java/io/github/symt/DiscordChatMod.java
+++ src/main/java/io/github/symt/DiscordChatMod.java
@@ -27,7 +27,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = DiscordChatMod.MODID, version = DiscordChatMod.VERSION)
 public class DiscordChatMod {
 
-  public static final String VERSION = "1.2.0";
+  public static final String VERSION = "1.4.0";
   public static final IChatComponent newLine = new ChatComponentTranslation("", new Object[0]);
   public static final String TOKEN_PATH = "token.txt";
   static final String MODID = "DiscordChatMod";
